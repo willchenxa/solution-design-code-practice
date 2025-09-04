d942deb86031e63b7499c04dbd71fbb2fe4778c5
Wei Chen <williamchen@Weis-MacBook-Pro.local>
2025-09-04 19:20:06 +1000

    largest rectangle

---
+++ Result/Program.cs
@@ -0,0 +1,13 @@
+﻿using Result.LargestRectangle;
+/************* Largest Rectangle *************/
+
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+List<int> h = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(hTemp => Convert.ToInt32(hTemp)).ToList();
+
+long result = LargestRectangle.largestRectangle(h);
+
+Console.WriteLine(result);
+Console.Clear();
+/************* Largest Rectangle *************/
+
